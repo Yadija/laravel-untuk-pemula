9d459c3e922a2e2d7f2dd841ef815904e60c9670
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-06-14 22:58:01 +0800

    ⚒️build: make post

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//route resource
+Route::resource('/posts', \App\Http\Controllers\PostController::class);
